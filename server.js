d9b64f684126232004872c947e5a196b61800a6e
Shidhin C R <shidhincr@gmail.com>
2016-11-29 11:51:45 +0530

    Enable gZip

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+var compression = require('compression');
 var express = require('express');
 var app = express();
 
+app.use(compression());
 app.use(express.static('public'));
 app.use('/dist', express.static('dist'));
 
